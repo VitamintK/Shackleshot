100f0173c283179b6dda7aa99d70af05cdf23bc8
KEvin Wang <kvn.a.wng@gmail.com>
2013-06-20 16:17:02 -0700

    what am I supposed to write here

--- Shackleshot.py
+++ Shackleshot.py
@@ -1,35 +1,37 @@
 import requests
 import xml.etree.ElementTree as ET
 
-improductive= []
 ia=0
+sequence = 0
+#maxmatch = "9999999999"
+maxmatch = "12822135"
+go = True
 #def getHistory():
 
-while True:
+while go == True:
+    sequence+=1
+    maxmatch = str(int(maxmatch)-1)
     r = requests.get("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?"
                      "format=%s"
                      "&key=%s"
                      "&account_id=%s"
-                     "&matches_requested=%s"%("XML","9D6AA7810AF5EF66B3A70566614DE147","40753485","60"))
+                     "&matches_requested=%s"
+                     "&start_at_match_id=%s"%("XML","9D6AA7810AF5EF66B3A70566614DE147","40753485","60",maxmatch))
     tree = ET.fromstring(r.text.encode('ascii', 'ignore'))
     matches=tree.find("matches").findall("match")
+    if matches:
+        go = False
     for h in matches:
         ia+=1
-        fdsa = h.find("players").findall("player")
+        #fdsa = h.find("players").findall("player")
         templist = []
+        if h.find("match_id").text == maxmatch:
+            go = False
         maxmatch = h.find("match_id").text
-        """for i in fdsa:
-            #improductive.append(i.find("level").text)
-            try:
-                if i.find("account_id").text == "4294967295":
-                    templist.append("1")
-                else:
-                    templist.append( i.find("account_id").text)
-            except:
-                templist.append("0")"""
         #print templist
         print maxmatch
-    break
+    #if sequence == 3:
+    #break
 #print r.text
-print improductive
 print ia
+print "sequence " + str(sequence)
