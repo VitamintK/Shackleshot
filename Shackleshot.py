8fecaef8e3200475693d3d915381d83e0c2b26ff
KEvin Wang <kvn.a.wng@gmail.com>
2013-06-27 05:02:13 -0700

    getallmatches and getallmatchdetails
    added functions to get all of my match IDs, and functions to get details
    of each match, and save each to files.

--- Shackleshot.py
+++ Shackleshot.py
@@ -1,35 +1,69 @@
 import requests
+import os
+import pickle
 import xml.etree.ElementTree as ET
+def getAllMatches(playerid):
+    matchlist = []
+    ia=0
+    sequence = 0
+    maxmatch = "9999999999"
+    #maxmatch = "112822135"
+    go = True
 
-ia=0
-sequence = 0
-#maxmatch = "9999999999"
-maxmatch = "112822135"
-go = True
-#def getHistory():
+    while go == True:
+        sequence+=1
+        maxmatch = str(int(maxmatch)-1)
+        r = requests.get("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?"
+                         "format=%s"
+                         "&key=%s"
+                         "&account_id=%s"
+                         "&matches_requested=%s"
+                         "&start_at_match_id=%s"%("XML","9D6AA7810AF5EF66B3A70566614DE147",playerid,"60",maxmatch))
+        tree = ET.fromstring(r.text.encode('ascii', 'ignore'))
+        matches=tree.find("matches").findall("match")
+        if not matches:
+            go = False
+        for h in matches:
+            ia+=1
+            #fdsa = h.find("players").findall("player")
+            templist = []
+            maxmatch = h.find("match_id").text
+            #print templist
+            matchlist.append(maxmatch)
+            print maxmatch
+    #print r.text
+    print "matches: " + str(ia)
+    print "sequence: " + str(sequence)
+    return matchlist
+def saveAllMatches():
+    allmatches = getAllMatches("40753485")
+    #print allmatches
+    with open("matchlist.txt",'w') as p:
+        pickle.dump(allmatches,p)
 
-while go == True:
-    sequence+=1
-    maxmatch = str(int(maxmatch)-1)
-    r = requests.get("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?"
-                     "format=%s"
-                     "&key=%s"
-                     "&account_id=%s"
-                     "&matches_requested=%s"
-                     "&start_at_match_id=%s"%("XML","9D6AA7810AF5EF66B3A70566614DE147","40753485","60",maxmatch))
-    tree = ET.fromstring(r.text.encode('ascii', 'ignore'))
-    matches=tree.find("matches").findall("match")
-    if not matches:
-        go = False
-    for h in matches:
-        ia+=1
-        #fdsa = h.find("players").findall("player")
-        templist = []
-        maxmatch = h.find("match_id").text
-        #print templist
-        print maxmatch
-    #if sequence == 3:
-    #break
-#print r.text
-print ia
-print "sequence " + str(sequence)
+def getAllDetails(matchfile):
+    allDetailsXML = []
+    with open(matchfile, 'r') as f:
+        matchlist = pickle.load(f)
+    for i in matchlist:
+        r = requests.get("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/"
+                         "?format=%s"
+                         "&key=%s"
+                         "&match_id=%s"%("XML","9D6AA7810AF5EF66B3A70566614DE147",i))
+        allDetailsXML.append(r.text)
+        #print r.text
+        print i
+    return allDetailsXML
+
+def saveAllDetails():
+    alldetails = getAllDetails("matchlist.txt")
+    with open("matchdetails.txt",'w') as p:
+        pickle.dump(alldetails,p)
+
+def openDetails():
+    with open("matchdetails.txt", 'r') as f:
+        matchdetails = pickle.load(f) 
+    return matchdetails
+
+matchdetails = openDetails()
+print matchdetails[0]
