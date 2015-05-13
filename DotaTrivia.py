4d286eeff7528b1458620ec8fc453575954d4ba4
KEvin Wang <kevinwang@kevinwang.us>
2015-05-13 03:32:45 -0700

    making a flask app

--- DotaTrivia.py
+++ DotaTrivia.py
@@ -4,46 +4,58 @@ import random
 import requests
 import xml.etree.ElementTree as ET
 
-matchez = Shackleshot.openDetails(Shackleshot.mabufula)
-random_match = random.choice(matchez)
-#Shackleshot.printMatchSummary(random_match)
-tree = ET.fromstring(random_match.encode('ascii', 'ignore'))
-players = tree.find("players").findall("player")
-random_player = random.choice(players)
+def random_trivia():
+    matchez = Shackleshot.openDetails(Shackleshot.mabufula)
+    random_match = random.choice(matchez)
+    #Shackleshot.printMatchSummary(random_match)
+    tree = ET.fromstring(random_match.encode('ascii', 'ignore'))
+    players = tree.find("players").findall("player")
+    random_player = random.choice(players)
 
-children = random_player.getchildren()
-for child in children:
-    ctag = child.tag
-    ctext = child.text
-    if ctag[:4]=="item": #can replace with case but how 2 do in python?
-        if ctext == '0':
-            print("")
-        else:
-            print("    {}".format(itemray[ctext]))
-    elif ctag == "account_id":
-        try:
-            r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?"
-                             "format=%s"
-                             "&key=%s"
-                             "&steamids=%s"%("XML",apikey,int(ctext)+76561197960265728))
-            tree = ET.fromstring(r.text.encode('ascii', 'ignore'))
-            player =tree.find("players").find("player")
+    pretty_string = ''
+    children = random_player.getchildren()
+    for child in children:
+        ctag = child.tag
+        ctext = child.text
+        if ctag[:4]=="item": #can replace with case but how 2 do in python?
+            if ctext == '0':
+                pretty_string +=("") + '\n'
+            else:
+                pretty_string +=("    {}".format(itemray[ctext])) + '\n'
+        elif ctag == "account_id":
             try:
-                print(player.findtext('personaname')) 
+                r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?"
+                                 "format=%s"
+                                 "&key=%s"
+                                 "&steamids=%s"%("XML",apikey,int(ctext)+76561197960265728))
+                tree = ET.fromstring(r.text.encode('ascii', 'ignore'))
+                player =tree.find("players").find("player")
+                try:
+                    pretty_string += (player.findtext('personaname'))  + '\n'
+                except:
+                    pretty_string += ("???????") + '\n'
             except:
-                print("???????")
-        except:
-            raise
-            print("error retrieving player name. check net connection")
-    elif ctag == "hero_id":
-        
-        #DO NOT OUTPUT HERO NAME.  This is for players to guess.
-        random_hero = Shackleshot.getHero(ctext) #can streamline this with getAllHeroes
-    else:
-        print(ctag + ": " + ctext)#most are self-explanatory
+                raise
+                pretty_string += ("error retrieving player name. check net connection") + '\n'
+        elif ctag == "hero_id":
+            
+            #DO NOT OUTPUT HERO NAME.  This is for players to guess.
+            random_hero = Shackleshot.getHero(ctext) #can streamline this with getAllHeroes
+        else:
+            pretty_string += (ctag + ": " + ctext) + '\n'#most are self-explanatory
+    return pretty_string
+    #guess = input("What hero was this? (case insensitive) ")
+    #if random_hero.lower() == guess.lower():
+    #    print("YOU WERE RIGHT")
+    #else:
+    #    print("YOU WERE WRONG.  The hero was {}".format(random_hero.lower()))
 
-guess = input("What hero was this? (case insensitive) ")
-if random_hero.lower() == guess.lower():
-    print("YOU WERE RIGHT")
-else:
-    print("YOU WERE WRONG.  The hero was {}".format(random_hero.lower()))
+if __name__ == '__main__':
+    print(random_trivia())
+    guess = input("What hero was this? (case insensitive) ")
+    if random_hero.lower() == guess.lower():
+        print("YOU WERE RIGHT")
+    else:
+        print("YOU WERE WRONG.  The hero was {}".format(random_hero.lower()))
+
+#more info needed: date, gamelength, etc.
---
+++ DotaTriviaFlaskApp.py
@@ -0,0 +1,10 @@
+import DotaTrivia
+from flask import Flask, render_template, request, jsonify, session
+app = Flask(__name__)
+
+@app.route('/')
+def trivia():
+    return DotaTrivia.random_trivia()
+
+if __name__ == '__main__':
+    app.run(debug = True)#host = '0.0.0.0')
